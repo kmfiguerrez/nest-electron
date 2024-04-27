d82adad9aed7954c89b331c2501bb96fdecb1a4e
kmfiguerrez <kmfiguerrez@gmail.com>
2024-04-27 08:17:14 +0800

    Add nestjs dto.

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,12 +1,14 @@
-import { Controller, Post } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
 import { AuthService } from "./auth.service";
+import { IAuthDTO } from "./dto";
 
 @Controller('api/auth')
 export class AuthController{
   constructor(private authService: AuthService) {}
 
   @Post('signup')
-  signup() {
+  signup(@Body() dto: IAuthDTO) {
+    console.dir(dto)
     return this.authService.signup()
   }
 
